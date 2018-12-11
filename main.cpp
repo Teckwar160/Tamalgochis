f415b34156c2a818559352f6906ed3e8ef7168c1
Ricardo Peña <teckwar160@gmail.com>
2018-12-11 11:46:42 -0600

    Creacion del menu principal

--- main.cpp
+++ main.cpp
@@ -1,9 +1,52 @@
 #include <iostream>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string>
+#include <locale>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+//Variables
+string notificacion = "Escoge una opción";
+char decision;
+
+//Prototipos
+
+//Main
+
+int main(){
+    setlocale(LC_CTYPE,"Spanish");
+
+    do{
+        system("cls");
+        cout << "\n\n\tTamalgochis\n\n";
+
+        cout << "Notificacion: " << notificacion << endl
+             << "a) Crear un Tamalgochi" << endl
+             << "b) Entrar al juego" << endl
+             << "c) Salir" << endl
+             << "=> ";
+        cin >> decision;
+
+        decision = toupper(decision);
+
+        switch (decision){
+            case 'A':
+                notificacion = "";
+                break;
+            case 'B':
+                notificacion = "";
+                break;
+            case 'C':
+                break;
+            default:
+            notificacion = "Opción invalida";
+            break;
+        }
+        system("cls");
+    }while(decision != 'C');
+
+
 }
+
+//Funcioens
