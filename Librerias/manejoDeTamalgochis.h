9b25aa7cf401656e80bc6f4083b68caeefd9a99e
Ricardo Peña <teckwar160@gmail.com>
2018-12-12 06:34:46 -0600

    Lista funcion crear

--- Librerias/manejoDeTamalgochis.h
+++ Librerias/manejoDeTamalgochis.h
@@ -27,12 +27,12 @@ string Fmensaje();
 
 void crear(){
     //Variables de Crear
-    string notificacion = "No uses espacios, estos se ignoraran";
-    char nombre[20],nombreLista[20],verificacion;
-    string nombreString,Direccion;
-    int j,a=0,largo;
     ofstream archivo;
     ifstream buscador;
+    string notificacion = "No uses espacios, estos se ignoraran";
+     string nombreString,Direccion,DireccionNumeroInventario,DireccionInventario;
+    char nombre[50],nombreLista[50],nombreInventario[50],numeroInventario[50],verificacion;
+    int j,a=0,largo;
     bool libre;
 
     //Inicio de la funcion
@@ -56,6 +56,7 @@ void crear(){
 
         strcpy(nombreLista,nombreString.c_str());
 
+
         largo = strlen(nombreLista);
         while(nombreLista[a]!='\0'){
             if (nombreLista[a]==' '){
@@ -75,11 +76,19 @@ void crear(){
 
         //Quita los espacios del nombre para evitar problemas
 
+
+        DireccionNumeroInventario = Direccion + nombreString + "numeroInventario.txt";
+        DireccionInventario = Direccion + nombreString + "inventario.txt";
         Direccion = Direccion + nombreString + ".txt";
 
-
+        //Direccion del archivo
         strcpy(nombre,Direccion.c_str());
+        //Direccion del numero de inventario
+        strcpy(numeroInventario,DireccionNumeroInventario.c_str());
+        //Direccion del inventario
+        strcpy(nombreInventario,DireccionInventario.c_str());
 
+        //Quita los espacios de la direccion
         largo = strlen(nombre);
         while(nombre[a]!='\0'){
             if (nombre[a]==' '){
@@ -91,6 +100,8 @@ void crear(){
             a++;
         }
 
+        //Comprueba si es correcto el nombre
+
         do{
             cout << "\n¿Es correcto? s/n" << endl
                  << "=> ";
@@ -99,41 +110,54 @@ void crear(){
             verificacion = toupper(verificacion);
         }while(verificacion != 'S' && verificacion != 'N');
 
+        //En caso de serlo
+
         if (verificacion == 'S'){
 
-        buscador.open(nombre,ios::in);
-        if(!buscador.fail()){
-            notificacion = "El nombre ya esta en uso";
-            libre = false;
-        }else{
-            libre = true;
-        }
-        buscador.close();
+            buscador.open(nombre,ios::in);
+            if(!buscador.fail()){
+                notificacion = "El nombre ya esta en uso";
+                libre = false;
+            }else{
+                libre = true;
+            }
+            buscador.close();
 
 
-        if(libre == true){
-            archivo.open(nombre,ios::app);
-            archivo << nombreLista << " " << vida << " " << ataque << " " << defensa << " " << inventario << endl;
-            archivo.close();
+            if(libre == true){
 
-            //Lista de todos los Tamalgochis
+                //Crea los archivos de un tamagochi
+                archivo.open(nombre,ios::app);
+                archivo << nombreLista << " " << vida << " " << ataque << " " << defensa << endl;
+                archivo.close();
 
-            archivo.open("Archivos/Tamalgochis.txt",ios::app);
-            archivo << nombreLista << endl;
-            archivo.close();
+                //Guarda el numero de inventario que tendra
 
-            RTnotificacion = "Tamalgochi creado con exito";
+                archivo.open(numeroInventario,ios::app);
+                archivo << inventario;
+                archivo.close();
 
-        }
+                //Crear el inventario
+
+                archivo.open(nombreInventario,ios::app);
+                archivo.close();
+
+
+                //Lista de todos los Tamalgochis
+
+                archivo.open("Archivos/Tamalgochis.txt",ios::app);
+                archivo << nombreLista << endl;
+                archivo.close();
+
+                RTnotificacion = "Tamalgochi creado con exito";
+
+            }
         }else{
             system("cls");
         }
+        system("cls");
 
     }while(libre != true);
-
-    fflush(stdin);
-
-
 
 }
 
